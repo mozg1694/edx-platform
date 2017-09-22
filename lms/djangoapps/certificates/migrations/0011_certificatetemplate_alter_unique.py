6f8212f2462acdda588768953dc2d2391df83721
Michael LoTurco <mikeloturco13@gmail.com>
2017-09-22 14:30:38 -0400

    added logging to reverse migration

--- lms/djangoapps/certificates/migrations/0011_certificatetemplate_alter_unique.py
+++ lms/djangoapps/certificates/migrations/0011_certificatetemplate_alter_unique.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
-from __future__ import unicode_literals
+import logging
 
+from __future__ import unicode_literals
 from django.db import migrations, models
+
+log = logging.getLogger(__name__)
 
 def revert_alter_unique(apps, schema_editor):
     CertificateTemplateModel = apps.get_model("certificates", "CertificateTemplate")
@@ -13,13 +16,13 @@ def revert_alter_unique(apps, schema_editor):
         "course_key",
         "mode").distinct()
 
-    # for each set, of org_id, course_key and mode
+    # for each course specific template by org_id, course_key and mode
     for org_id, course_key, mode in all_unique_templates_ignoring_language:
         course_templates = CertificateTemplateModel.objects.filter(organization_id=org_id, course_key=course_key, mode=mode)
-        if course_templates.count() > 1 #theres more than one template for a course
+        if course_templates.count() > 1  # theres more than one template for a course
             language_specific_course_templates = course_templates.exclude(language=None)
-            # for template in language_specific_course_templates:
-            #     log.info('Deleting ' + template.language + ' language template for course '+template.course_key)
+            for template in language_specific_course_templates:
+                log.info('Deleting ' + template.language + ' language template for course '+template.course_key)
             CertificateTemplateModel.objects.filter(id__in=list(language_specific_course_templates)).delete()
 
 
