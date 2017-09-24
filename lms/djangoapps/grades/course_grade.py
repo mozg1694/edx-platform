135aa3223b82ab014090961b29019c7578a4dc1a
Nimisha Asthagiri <nasthagiri@edx.org>
2017-09-24 19:31:29 -0400

    Grades: Clean up tests EDUCATOR-1404

--- lms/djangoapps/grades/course_grade.py
+++ lms/djangoapps/grades/course_grade.py
@@ -13,10 +13,6 @@ from .subsection_grade import ZeroSubsectionGrade
 from .subsection_grade_factory import SubsectionGradeFactory
 
 
-def uniqueify(iterable):
-    return OrderedDict([(item, None) for item in iterable]).keys()
-
-
 class CourseGradeBase(object):
     """
     Base class for Course Grades.
@@ -173,7 +169,7 @@ class CourseGradeBase(object):
         """
         return [
             self._get_subsection_grade(course_structure[subsection_key])
-            for subsection_key in uniqueify(course_structure.get_children(chapter_key))
+            for subsection_key in set(course_structure.get_children(chapter_key))
         ]
 
     @abstractmethod
